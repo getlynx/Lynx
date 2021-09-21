885fd7001a8e7d17bc7f37236603817195fe2bcc
Benjamin Wilson <b@getlynx.io>
2021-09-21 18:46:02 -0400

    Prior to wallet build, new checkpoints

--- contrib/checkpointgenerator.sh
+++ contrib/checkpointgenerator.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 for i in $(seq 1 1 5); do echo "            (  $i, uint256S(\"0x$(lynx-cli getblockhash $i)\"))"; done
 for i in $(seq 500000 500000 2949999); do echo "            (  $i, uint256S(\"0x$(lynx-cli getblockhash $i)\"))"; done
-for i in $(seq 2979995 1 2980000); do echo "            (  $i, uint256S(\"0x$(lynx-cli getblockhash $i)\"))"; done
+for i in $(seq 2980895 1 2980900); do echo "            (  $i, uint256S(\"0x$(lynx-cli getblockhash $i)\"))"; done
