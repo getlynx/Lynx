80fa12375922fd725bbdd25f0388e6129a414393
TheCharlatan <seb.kung@gmail.com>
2023-05-09 22:03:50 +0200

    refactor: Use ChainType enum exhaustively
    This is a follow up of https://github.com/bitcoin/bitcoin/pull/27491,
    more concretely
    https://github.com/bitcoin/bitcoin/pull/27491#discussion_r1188847896,
    for not using default cases (as per the style guide), and
    https://github.com/bitcoin/bitcoin/pull/27491#discussion_r1188852707 and
    https://github.com/bitcoin/bitcoin/pull/27491#discussion_r1188851857 for
    avoiding dead code.
    
    Also change chain name to chain type in docstrings

--- src/bitcoin-cli.cpp
+++ src/bitcoin-cli.cpp
@@ -432,9 +432,10 @@ private:
             return " signet";
         case ChainType::REGTEST:
             return " regtest";
-        default:
+        case ChainType::MAIN:
             return "";
         }
+        assert(false);
     }
     std::string PingTimeToString(double seconds) const
     {
