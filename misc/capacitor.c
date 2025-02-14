22fbdbc60bc22e9595608247f2ea6bfd1028cd53
Mitch Horton <mitchel.dewayne.horton@gmail.com>
2025-02-14 16:14:05 -0500

    Added error checking.

--- misc/capacitor.c
+++ misc/capacitor.c
@@ -3,11 +3,47 @@
 
 int main (int argc, char **argv) {
 
+
+
+  FILE *file = fopen ("capacitor.sh", "r");
+
+  if (file) {
+
+    fclose(file);
+
+  } else {
+
+    system ("logger -p user.error -t lynx -s capacitor capacitor.sh not found");
+
+    return 0;
+
+  }
+
+
+
+  if (argc == 1) {
+
+    system ("logger -p user.error -t lynx -s capacitor no command line argument");
+
+    return 0;
+
+  }
+
+
+
   char command[256];
 
   snprintf(command, sizeof(command), "/root/capacitor.sh %d &", atoi (argv[1]));
 
   system (command);
+
+
+
+
+
+
+
+
 
   return 0;
 
