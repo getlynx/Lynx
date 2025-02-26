d2a06ab8ad1ca93b0bc08d42d0b98bb5b275ef13
Benjamin Wilson <ben@getlynx.io>
2025-02-26 00:37:36 +0000

    Undo test

--- src/base58.cpp
+++ src/base58.cpp
@@ -14,7 +14,6 @@
 
 #include <limits>
 
-
 /** All alphanumeric characters except for "0", "I", "O", and "l" */
 static const char* pszBase58 = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
 static const int8_t mapBase58[256] = {
