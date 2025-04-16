8718bdbfb8e6f86aaa6789b231ecc713d78523a9
Mitch Horton <mitchel.dewayne.horton@gmail.com>
2025-04-16 12:34:53 -0400

    test

--- src/storage/auth.cpp
+++ src/storage/auth.cpp
@@ -2,6 +2,8 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
+// Test
+
 #define TIMING 1
 
 #include <storage/auth.h>
