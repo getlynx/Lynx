4610d8576fa8aa60d97d12665dc1b48769c735b3
Benjamin Wilson <ben@getlynx.io>
2026-07-14 23:12:04 -0400

    spark version

--- contrib/installer/install.sh
+++ contrib/installer/install.sh
@@ -6,7 +6,7 @@ PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 set -euo pipefail
 
 # Installer version (x.x.x format)
-SPARK_INSTALLER_VERSION="2.13.0"
+SPARK_INSTALLER_VERSION="2.14.0"
 
 # Parse command-line arguments
 chain_name=""
